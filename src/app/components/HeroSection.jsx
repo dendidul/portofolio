c5e0e5aa71dfa5922db6f084f96edec96233a245
erdendi <dendidul@gmail.com>
2024-10-20 09:37:56 +0700

    change image hero

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Dendi",
+                "Megalo",
                 1000,
                
                 "Full Stack Developer",
@@ -63,7 +63,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="./images/hero-image-dendi.png"
+              src="./images/hero-image-megalo-round.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={400}
